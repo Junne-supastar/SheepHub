23d02000938b153c5c86f0ad9319a8a7cb70464e
Julianne <juliannepargasantos11@gmail.com>
2025-09-03 22:22:54 -0300

    Removendo arquivos do repositório, mantendo localmente

--- resources/views/partials/topbar.blade.php
+++
@@ -1,6 +0,0 @@
-<input type="search" placeholder="Pesquisar" class="pesquisa" />
-<img src="{{ asset('imgfeed/Vector.png') }}" alt="Lupa" class="lupa" />
-<div class="user-info">
-  <img src="{{ asset('img/fotodeperfil.png') }}" alt="User Avatar" />
-  <span>{{ auth()->user()->name ?? 'Usuário' }}</span>
-</div>
--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-use App\Http\Controllers\Api\UsuarioApiController;
-
-Route::post('/usuario/cadastrar', [UsuarioController::class, 'cadastrar']);
-Route::post('/usuario/login', [UsuarioController::class, 'login']);
-
-?>
