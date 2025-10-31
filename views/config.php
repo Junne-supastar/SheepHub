d138696e85ba4d8ca70e0b19caa16ae132e20c8e
Ana Clara <anaclara.duarte0707@gmail.com>
2025-10-31 16:48:52 -0300

    Add files via upload

---
+++ views/config.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Configuração</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
+    <link rel="stylesheet" href="../assets/css/config.css">
+</head>
+
+<body id="body-pd">
+<?php include __DIR__ . '/../includes/sidebar.php'; ?>
+
+    <div class="container-geral">
+
+        <div class="bloco01">
+            <div class="area01-bloco01">
+                <h1>Configurações e atividade</h1>
+
+                <div class="search-bloco01">
+                    <input type="text" placeholder="Buscar">
+
+                </div>
+            </div> <!--area01-->
+
+            <div class="area02-bloco01">
+                <h3>Como você usa a SheepHub</h3>
+
+                <div class="cards-area02-bloco01">
+
+                    <div class="card-area02-bloco01" id="card01-bloco2">
+                        <i class="bi bi-person-circle"></i>
+                        <p>Central da conta</p>
+                    </div>
+
+                    <div class="card-area02-bloco01" id="card02-bloco2">
+                        <i class="bi bi-folder-fill"></i>
+                        <p>Posts arquivados</p>
+                    </div>
+
+                    <div class="card-area02-bloco01" id="card03-bloco2">
+                        <i class="bi bi-bookmark-fill"></i>
+                        <p>Salvamentos</p>
+                    </div>
+
+                </div> <!--cards-area02-bloco01-->
+            </div> <!--area02-bloco01-->
+
+        </div> <!--Bloco 01-->
+
+
+
+
+        <div class="bloco02">
+
+            <div class="sessoes-bloco02">
+
+                <div class="area-cont-bloco2">
+                    <i class="bi bi-chat-fill"></i>
+                    <div class="cont-sessa0-bloco02">
+                        <h3>Comentários</h3>
+                        <p>Gerencie e interaja com os comentários nas suas postagens. </p>
+                    </div>
+                </div>
+
+                <div class="area-seta-bloco2">
+                    <i class="bi bi-chevron-right"></i>
+                </div>
+            </div>
+
+
+            <div class="sessoes-bloco02">
+
+                <div class="area-cont-bloco2">
+                    <i class="bi bi-bookmark-star-fill"></i>
+                    <div class="cont-sessa0-bloco02">
+                        <h3>Favoritos</h3>
+                        <p>Acesse rapidamente suas postagens e conteúdos favoritos. </p>
+                    </div>
+                </div>
+
+                <div class="area-seta-bloco2">
+                    <i class="bi bi-chevron-right"></i>
+                </div>
+            </div>
+
+
+            <div class="sessoes-bloco02">
+
+                <div class="area-cont-bloco2">
+                    <i class="bi bi-emoji-smile-fill"></i>
+                    <div class="cont-sessa0-bloco02">
+                        <h3>Ajuda</h3>
+                        <p>Encontre respostas rápidas e suporte para suas dúvidas e problemas. </p>
+                    </div>
+                </div>
+
+                <div class="area-seta-bloco2">
+                    <i class="bi bi-chevron-right"></i>
+                </div>
+            </div>
+
+            <div class="sessoes-bloco02">
+
+                <div class="area-cont-bloco2">
+                    <i class="bi bi-phone-landscape-fill"></i>
+                    <div class="cont-sessa0-bloco02">
+                        <h3>Pedidos e pagamentos</h3>
+                        <p>Gerencie suas compras, pedidos e informações de pagamento. </p>
+                    </div>
+                </div>
+
+                <div class="area-seta-bloco2">
+                    <i class="bi bi-chevron-right"></i>
+                </div>
+            </div>
+
+            <div class="sessoes-bloco02">
+
+                <div class="area-cont-bloco2">
+                    <i class="bi bi-shield-lock-fill"></i>
+                    <div class="cont-sessa0-bloco02">
+                        <h3>Central de privacidade</h3>
+                        <p>Controle suas configurações de privacidade e segurança. </p>
+                    </div>
+                </div>
+
+                <div class="area-seta-bloco2">
+                    <i class="bi bi-chevron-right"></i>
+                </div>
+            </div>
+
+            <div class="sessoes-bloco02">
+
+                <div class="area-cont-bloco2">
+                    <i class="bi bi-info-circle-fill"></i>
+                    <div class="cont-sessa0-bloco02">
+                        <h3>Sobre</h3>
+                        <p>Saiba mais sobre nossa plataforma, missão e equipe. </p>
+                    </div>
+                </div>
+
+                <div class="area-seta-bloco2">
+                    <i class="bi bi-chevron-right"></i>
+                </div>
+            </div>
+
+        </div> <!--Bloco 02-->
+
+
+
+        <footer>
+            <div class="links-footer">
+                <a href="#">Sobre</a>
+                <a href="#">Ajuda</a>
+                <a href="#">Termos</a>
+                <a href="#">Carreiras</a>
+                <a href="#">Privacidade</a>
+            </div>
+
+            <div class="lmtd">
+                <p> © 2024 SHEEPHUB FROM META</p>
+            </div>
+        </footer>
+
+
+
+
+
+
+
+
+
+    </div> <!--Container geral-->
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
